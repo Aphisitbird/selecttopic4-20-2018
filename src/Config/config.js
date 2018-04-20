1b9bfc5bea789a85563b3217fb6081ee26ecd447
Aphisitbird <bird_benjama1@hotmail.com>
2018-04-20 12:02:18 +0700

    first commit

---
+++ src/Config/config.js
@@ -0,0 +1,9 @@
+export const DB_CONFIG ={
+    apiKey: "AIzaSyB_P3QwZmG1o5bASOPlC4z5Jc-NRjOIzw4",
+    authDomain: "selecttopic-870a7.firebaseapp.com",
+    databaseURL: "https://selecttopic-870a7.firebaseio.com",
+    projectId: "selecttopic-870a7",
+    storageBucket: "selecttopic-870a7.appspot.com",
+    messagingSenderId: "698900958542"
+
+  }
